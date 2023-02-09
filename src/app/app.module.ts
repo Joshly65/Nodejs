b69c2ddba3b8a43dbd64e92a1557a7def27dc126
Joshly <johassnparra65@gmail.com>
2023-02-08 19:45:36 -0500

    nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibroComponent } from './libro/libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibroComponent
   ],
   imports: [
     BrowserModule,
